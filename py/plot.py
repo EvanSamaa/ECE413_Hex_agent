7ab805e022ebe73585bf0e4faa0f863d7c886bf4
Jonah <jonah@biorender.com>
2021-04-19 18:20:50 -0400

    Start on plots

--- py/plot.py
+++ py/plot.py
@@ -1,9 +1,30 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-path = '../runs/7x7_transfer_final/eval-0.txt'
-data = np.loadtxt(path, delimiter=',')
+# Plots of win rate versus training iterations
+training_plots = [
+    {
+        'title': '7x7',
+        'models': ['7x7_final', '7x7_transfer_final'],
+        'eval-iterations': [0, 100],
+    },
+    {
+        'title': '8x8',
+        'models': ['8x8_Raw_final', '8x8_transfer_final'],
+        'eval-iterations': [0, 100],
+    },
+]
 
-plt.plot(data[:, 0], data[:, 1])
-plt.ylim([0,1])
-plt.savefig('test.png')
+for plot_config in training_plots:
+    plt.figure()
+    for its in plot_config['eval-iterations']:
+        for model in plot_config['models']:
+            path = '../runs/{}/eval-{}.txt'.format(model, its)
+            data = np.loadtxt(path, delimiter=',')
+            plt.plot(data[:, 0], data[:, 1], label='{} vs {}'.format(model, 'random' if its == 0 else 'mcts'))
+    plt.ylim([0,1])
+    plt.title(plot_config['title'])
+    plt.xlabel('Self play iterations')
+    plt.ylabel('Win rate against baseline')
+    plt.legend(loc="center right")
+    plt.savefig('../plots/{}.svg'.format(plot_config['title']))
